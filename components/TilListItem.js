7a17e662c30ffbfd7df4adc1036989e6759c793b
soniczsonic <kabaya.dori@gmail.com>
2019-05-19 12:34:27 +0900

    style: added padding to List item

--- components/TilListItem.js
+++ components/TilListItem.js
@@ -12,56 +12,36 @@ import {
 } from 'react-native'
 
 const TilListItem = (props) => (
+    <FlatList
+      data={props.til}
+      renderItem={({ item }) => 
+        <View style={styles.list}>
+          <Text
+            style={styles.contentText}
+            numberOfLines={5}
+          >
+            {item.tilContentText}
+          </Text>
+        </View>
+      }
+    />
+)
 
-
-        <FlatList
-          data={props.til}
-          renderItem={({ item }) => <View style={styles.list}>
-            <Text>{item.tilContentText}</Text>
-          </View>}
-        />
-
-    )
-
-    export default TilListItem
+export default TilListItem
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  heading: {
-    height: 60,
-    backgroundColor: '#03A9F4',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  headingTest: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-  },
   list: {
     margin: 5,
     backgroundColor: 'white',
-    height:80,
+    minHeight: 60,
     justifyContent: 'space-around',
-    paddingLeft: 10,
-    elevation: 1
+    paddingHorizontal: 25,
+    paddingVertical: 5,
+    elevation: 1,
   },
-  fab: {
-    position: 'absolute',
-    width: 56,
-    height: 56,
-    alignItems: 'center',
-    justifyContent: 'center',
-    right: 20,
-    bottom: 20,
-    backgroundColor: '#03A9F4',
-    borderRadius: 30,
-    elevation: 8
-  },
-  fabIcon: {
-    fontSize: 40,
-    color: 'white'
+  contentText: {
+    fontSize: 20,
+    lineHeight: 25,
+    letterSpacing: 1
   }
-});
+})
