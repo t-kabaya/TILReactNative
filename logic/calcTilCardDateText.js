192462ba682a5de4dbafdde1f2b1d8149f06791e
IPG <ipg@IPGnoMacBook-puro.local>
2019-08-20 00:40:19 +0900

    feature: change text of postTime

--- logic/calcTilCardDateText.js
+++ logic/calcTilCardDateText.js
@@ -1,4 +1,6 @@
 import { format } from 'date-fns'
+import isToday from 'date-fns/is_today'
+import isYesterday from 'date-fns/is_yesterday'
 
 // TILを受け取り、dateTextを与えて返す処理。
 export const addFormattedPostTime = TILs => {
@@ -7,9 +9,20 @@ export const addFormattedPostTime = TILs => {
     // secondsなので、1000倍する
     const date = new Date(til.date.seconds * 1000)
 
+    let postTimeText = null
+    isToday(date)
+
+    if (isToday(date)) {
+      postTimeText = format(date, '今日　H:mm')
+    } else if (isYesterday(date)) {
+      postTimeText = format(date, '昨日 H:mm')
+    } else {
+      postTimeText = format(date, 'YYYY年M月D日 H:mm')
+    }
+
     return {
       tilContentText: til.tilContentText,
-      postTimeText: format(date, 'YYYY年M月D日 H:mm')
+      postTimeText
     }
   })
 
